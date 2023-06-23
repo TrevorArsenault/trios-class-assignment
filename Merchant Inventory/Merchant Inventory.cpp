4bf53454a524e3b3563d062b2fd6473517dddc85
Guysus <137240103+Guysus@users.noreply.github.com>
2023-06-23 15:00:17 -0700

    Add files via upload

--- Merchant Inventory/Merchant Inventory.cpp
+++ Merchant Inventory/Merchant Inventory.cpp
@@ -10,8 +10,12 @@ using namespace std;
 //Represent an item - done
 //
 
+{
+public: void()
+	For(int i = 3; i < this.inventoryContents.length; ++i)
 
-
+	this.inventoryContents[i] = 3;
+}
 
 class InventoryItem
 {
@@ -22,8 +26,14 @@ public:
 };
 
 
+
+
+
+
+
 	int main()
 	{
+
 
 
 		InventoryItem playerItems[3];
@@ -70,22 +80,76 @@ public:
 
 
 
-		int wallet = 10
 
+		bool isSoldOut = true
+			isSoldOut = 0
 
+			int wallet = 10
 
-		cout << "Merchant Item : " << npcItems[0].name << "\n";
-		cout << "Cost of Item : " << npcItems[0].cost << "\n\n";
-		cout << "Quantity of Item(s) : " << npcItems[0].quantity << "\n\n";
+			char buy = b;
+		char sell = s;
+		char leave = l;
 
-		cout << "Merchant Item : " << npcItems[1].name << "\n";
-		cout << "Cost of Item : " << npcItems[1].cost << "\n\n";
-		cout << "Quantity of Item(s) : " << npcItems[1].quantity << "\n\n";
-
-		cout << "Merchant Item : " << npcItems[2].name << "\n";
-		cout << "Cost of Item : " << npcItems[2].cost << "\n\n";
-		cout << "Quantity of Item(s) : " << npcItems[2].quantity << "\n\n";
 		
+
+
+
+
+
+		while (window, isOpen)
+		{
+			cout << "Welcome to the Merchants Inventory" << endl;
+			cout << "How can I help you?" << endl;
+			cout << "[b]uy   [s]ell   [l]eave" << endl;
+			cin >> Keyboard::isKeyPressed(Keyboard::)
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::esc))
+		{
+			window.close()
+		}
+
+		else (Keyboard::isKeyPressed(Keyboard::b))
+		{
+			cout << "What would you like to purchase?" << endl;
+			cout << "Merchant Item : " << npcItems[0].name << endl;
+			cout << "Cost of Item : " << npcItems[0].cost << endl;
+			cout << "Quantity of Item(s) : " << npcItems[0].quantity << "\n\n";
+
+			cout << "Merchant Item : " << npcItems[1].name << "\n";
+			cout << "Cost of Item : " << npcItems[1].cost << "\n";
+			cout << "Quantity of Item(s) : " << npcItems[1].quantity << "\n\n";
+
+			cout << "Merchant Item : " << npcItems[2].name << "\n";
+			cout << "Cost of Item : " << npcItems[2].cost << "\n";
+			cout << "Quantity of Item(s) : " << npcItems[2].quantity << "\n\n";
+
+		}
+
+		else (Keyboard::isKeyPressed(Keyboard::s))
+		{
+			cout << "What would you like to sell?" << endl;
+			cout << "Players Item : " << playerItems[0].name << endl;
+			cout << "Cost of Item : " << playerItems[0].cost << endl;
+			cout << "Quantity of Item(s) : " << playerItems[0].quantity << "\n\n";
+
+			cout << "Players Item : " << playerItems[1].name << "\n";
+			cout << "Cost of Item : " << playerItems[1].cost << "\n";
+			cout << "Quantity of Item(s) : " << playerItems[1].quantity << "\n\n";
+
+			cout << "Players Item : " << nplayerItems[2].name << "\n";
+			cout << "Cost of Item : " << playerItems[2].cost << "\n";
+			cout << "Quantity of Item(s) : " << playerItems[2].quantity << "\n\n";
+
+		}
+
+		else (Keyboard::isKeyPressed(Keyboard::l))
+		{
+			window.return()
+
+		}
+
+		; 
 
 
 
