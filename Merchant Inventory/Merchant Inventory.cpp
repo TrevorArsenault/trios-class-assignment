0f63cf395b44695824e163a7a2c8a1ccf5c9cdde
Guysus <137240103+Guysus@users.noreply.github.com>
2023-06-26 08:38:08 -0700

    Add files via upload

--- Merchant Inventory/Merchant Inventory.cpp
+++ Merchant Inventory/Merchant Inventory.cpp
@@ -1,205 +1,145 @@
+
 #include <iostream>
 #include <string>
 #include <vector>
 
 
-using namespace std;
+	using namespace std;
 
 
+	//Represent an item - done
+	
 
-//Represent an item - done
-//
-
-{
-public: void()
-	For(int i = 3; i < this.inventoryContents.length; ++i)
-
-	this.inventoryContents[i] = 3;
-}
-
-class InventoryItem
-{
-public:
-    float cost;
-    std::string name;
-    int quantity;
-};
+	class InventoryItem
+	{
+	private:
+		float cost;
+		std::string name;
+		int quantity;
 
 
+	public:
+
+		
+
+		InventoryItem()
+		{}
 
 
+		InventoryItem(float _cost, std::string _name, int _quantity)
+		{
+			cost = _cost;
+			name = _name;
+			quantity = _quantity;
+		}
 
+		void ChangeQuantity(int newQuantity)
+		{
+			quantity = newQuantity;
+		}
+	};
 
 
 	int main()
 	{
-
-
-
 		InventoryItem playerItems[3];
 
 
+		playerItems[0] = InventoryItem(5.0f, "Dagger", 1);
 
-		playerItems[0].cost = 5;
-		playerItems[0].name = "Dagger";
-		playerItems[0].quantity = 1;
+		playerItems[1] = InventoryItem(25.0f, "Ruby", 1);
 
-
-
-		playerItems[1].cost = 25;
-		playerItems[1].name = "Ruby";
-		playerItems[1].quantity = 1;
-
-
-
-		playerItems[2].cost = 10;
-		playerItems[2].name = "Medicine";
-		playerItems[2].quantity = 1;
-
-
+		playerItems[2] = InventoryItem(10.0f, "Medicine", 1);
+	
 
 		InventoryItem npcItems[3];
 
 
+		npcItems[0] = InventoryItem(5.0f, "Sword", 3);
+	
+		npcItems[1] = InventoryItem(30.0f, "Diamond", 3);
+	
+		npcItems[2] = InventoryItem(20.0f, "Gun", 1);
 
-		npcItems[0].cost = 5;
-		npcItems[0].name = "Sword";
-		npcItems[0].quantity = 3;
+		
+	
 
+		float playerWalletAmount = 10.0f;
 
-
-		npcItems[1].cost = 30;
-		npcItems[1].name = "Diamond";
-		npcItems[1].quantity = 2;
-
-
-
-		npcItems[2].cost = 20;
-		npcItems[2].name = "Gun";
-		npcItems[2].quantity = 1;
-
-
-
-
-		bool isSoldOut = true
-			isSoldOut = 0
-
-			int wallet = 10
-
-			char buy = b;
-		char sell = s;
-		char leave = l;
+		char choice;
+		 
 
 		
 
-
-
-
-
-		while (window, isOpen)
+		do
 		{
 			cout << "Welcome to the Merchants Inventory" << endl;
 			cout << "How can I help you?" << endl;
 			cout << "[b]uy   [s]ell   [l]eave" << endl;
-			cin >> Keyboard::isKeyPressed(Keyboard::)
-		}
-
-		if (Keyboard::isKeyPressed(Keyboard::esc))
-		{
-			window.close()
-		}
-
-		else (Keyboard::isKeyPressed(Keyboard::b))
-		{
-			cout << "What would you like to purchase?" << endl;
-			cout << "Merchant Item : " << npcItems[0].name << endl;
-			cout << "Cost of Item : " << npcItems[0].cost << endl;
-			cout << "Quantity of Item(s) : " << npcItems[0].quantity << "\n\n";
-
-			cout << "Merchant Item : " << npcItems[1].name << "\n";
-			cout << "Cost of Item : " << npcItems[1].cost << "\n";
-			cout << "Quantity of Item(s) : " << npcItems[1].quantity << "\n\n";
-
-			cout << "Merchant Item : " << npcItems[2].name << "\n";
-			cout << "Cost of Item : " << npcItems[2].cost << "\n";
-			cout << "Quantity of Item(s) : " << npcItems[2].quantity << "\n\n";
-
-		}
-
-		else (Keyboard::isKeyPressed(Keyboard::s))
-		{
-			cout << "What would you like to sell?" << endl;
-			cout << "Players Item : " << playerItems[0].name << endl;
-			cout << "Cost of Item : " << playerItems[0].cost << endl;
-			cout << "Quantity of Item(s) : " << playerItems[0].quantity << "\n\n";
-
-			cout << "Players Item : " << playerItems[1].name << "\n";
-			cout << "Cost of Item : " << playerItems[1].cost << "\n";
-			cout << "Quantity of Item(s) : " << playerItems[1].quantity << "\n\n";
-
-			cout << "Players Item : " << nplayerItems[2].name << "\n";
-			cout << "Cost of Item : " << playerItems[2].cost << "\n";
-			cout << "Quantity of Item(s) : " << playerItems[2].quantity << "\n\n";
-
-		}
-
-		else (Keyboard::isKeyPressed(Keyboard::l))
-		{
-			window.return()
-
-		}
-
-		; 
 
 
+			cin >> choice;
 
-		
+
+			if (choice == 'b')
+			{
+				cout << "What would you like to purchase?" << endl;
+				cout << "Merchant Item : " << endl;
+				cout << " [a] Sword......$5" << endl; 
+				cout << " [b] Diamond....$30" << endl;
+				cout << " [c] Gun........$20" << endl;
+
+
+				cin >> choice; 
+				switch (choice)
+				{
+				case 'a':
+					cout << "Item Purchased" << endl;
+					break;
+				case 'b':
+					cout << "Item Purchased" << endl;
+					break;
+				case 'c':
+					cout << "Item Purchased" << endl;
+					break;
+				default:
+					cout << "Sold Out!" << endl;
+
+				}
+			
+				
+			}
+			else if (choice == 's')
+			{
+				cout << "What would you like to sell?" << endl;
+				cout << "Players Item : " << endl;
+				cout << "[a] Dagger.....$5" << endl;
+				cout << "[b] Ruby.......$25" << endl;
+				cout << "[c] Medicine...$10" << endl;
+
+				cin >> choice;
+				switch (choice)
+				{
+				case 'a':
+					cout << "Item Sold" << endl;
+					break;
+				case 'b':
+					cout << "Item Sold" << endl;
+					break;
+				case 'c':
+					cout << "Item Sold" << endl;
+					break;
+				}
+
+			}
+			else (choice == 'l')
+		   ;{
+				return 0;
+			}
+
+
+		} while (choice != 'l');
+
 
 		return 0;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		
 	}
-
